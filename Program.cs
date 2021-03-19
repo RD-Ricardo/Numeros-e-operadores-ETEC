7cc120cfc8049e51071485d21abc74a2d574e78a
RD-Ricardo <ricardodias169@gmail.com>
2021-03-19 13:55:26 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace N_meros_e_operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            double num1, num2, resp;
+
+            Console.WriteLine("Cálculo da soma entre dois números");
+            Console.WriteLine("----------------------------------");
+            Console.Write("Digite o primeiro número:  ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o  segundo número:  ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            resp = num1 + num2 ; 
+            Console.Write($"Soma dos números: {resp}");
+        }
+    }
+}
